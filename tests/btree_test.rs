1e6f09f7650d2ea0ef73a151b668681b2b05c304
Abhishek <abhishekjha96500@gmail.com>
2025-10-12 22:15:12 +0530

    added test for height balance

--- tests/btree_test.rs
+++ tests/btree_test.rs
@@ -1,3 +1,4 @@
+use rust_dsa::binary_trees::height_balance_check::BalanceWithHeight;
 use rust_dsa::binary_trees::tree::Tree;
 
 #[test]
@@ -8,4 +9,13 @@ pub fn btree_traversal_test() {
     Tree::preorder_collect(&root, &mut result);
 
     assert_eq!(result, vec![1, 2, 4, 5, 3]);
+}
+
+#[test]
+pub fn btree_balance_test() {
+    let values = vec![1, 2, 3, 4, 5];
+    let root = Tree::from_values(values);
+    let result = BalanceWithHeight::is_balanced(&root);
+
+    assert_eq!(result, true);
 }
